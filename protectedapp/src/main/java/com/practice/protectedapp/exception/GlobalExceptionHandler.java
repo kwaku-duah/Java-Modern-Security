fada7129848eaf87361460a72025a9e79c03c61a
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-22 01:15:45 +0000

    feature: add global exception handler

---
+++ protectedapp/src/main/java/com/practice/protectedapp/exception/GlobalExceptionHandler.java
@@ -0,0 +1,12 @@
+package com.practice.protectedapp.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/*
+ * exception handling
+ */
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+}
