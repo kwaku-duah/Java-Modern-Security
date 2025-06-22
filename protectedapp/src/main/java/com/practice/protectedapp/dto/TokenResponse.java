ef553165191f4dc43dcb5c507905514bc6470888
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-22 00:47:47 +0000

    feature: add token response dto

---
+++ protectedapp/src/main/java/com/practice/protectedapp/dto/TokenResponse.java
@@ -0,0 +1,17 @@
+package com.practice.protectedapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/*
+ * token response dto
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenResponse {
+    private String accessToken;
+}
